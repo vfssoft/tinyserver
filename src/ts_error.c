4623eb1f4a7759b9c2e2e155f618a3fd8aa86967
zzrrxx <253951598@qq.com>
2024-07-26 22:21:12 +0800

    simplify the ts_error_t.

--- src/ts_error.c
+++ src/ts_error.c
@@ -1,18 +1,12 @@
 
 #include "ts_internal.h"
 
-static ts_error_t pre_def_errs[] = {
-    { TS_ERR_OUT_OF_MEMORY, (char*)"out of memory" }
-};
 
-static BOOL ts_error__is_pre_defined_err(int err) {
-  int pre_def_errs_len = sizeof(pre_def_errs) / sizeof(ts_error_t);
-  for (int i = 0; i < pre_def_errs_len; i++) {
-    if (pre_def_errs[i].err == err) {
-      return TRUE;
-    }
+static const char* ts_error_pre_def_err_msg(int err) {
+  switch (err) {
+    case TS_ERR_OUT_OF_MEMORY: return "out of memory";
+    default: return "unknown error";
   }
-  return FALSE;
 }
 
 void ts_error__init(ts_error_t* errt) {
@@ -21,43 +15,29 @@ void ts_error__init(ts_error_t* errt) {
 }
 
 void ts_error__reset(ts_error_t* errt) {
-  if (ts_error__is_pre_defined_err(errt->err)) {
-    errt->err = 0;
-    errt->msg = NULL;
-  } else {
-    errt->err = 0;
-    if (errt->msg) {
-      ts__free(errt->msg);
-    }
-    errt->msg = NULL;
+  errt->err = 0;
+  if (errt->msg) {
+    ts__free(errt->msg);
   }
+  errt->msg = NULL;
 }
 
 // For predefined errors
 void ts_error__set(ts_error_t* errt, int err) {
-  assert(ts_error__is_pre_defined_err(err));
-  ts_error__reset(errt);
-
-  errt->err = err;
-  errt->msg = pre_def_errs[err].msg;
+  ts_error__set_msg(errt, err, ts_error_pre_def_err_msg(err));
 }
 
 // For customer errors
 void ts_error__set_msg(ts_error_t* errt, int err, const char* msg) {
-  assert(!ts_error__is_pre_defined_err(err));
   ts_error__reset(errt);
-
   errt->err = err;
   errt->msg = ts__strdup(msg);
 }
 
 // For customer errors
 void ts_error__set_msgf(ts_error_t* errt, int err, const char* format, ...) {
-  assert(!ts_error__is_pre_defined_err(err));
   ts_error__reset(errt);
-
-
-
+  
   char buf[1024];
   va_list args;
   va_start(args, format);
