5605c01a45a3b158f04183a152501485ea552b27
zzrrxx <253951598@qq.com>
2024-07-30 22:04:19 +0800

    Remove data_pipe
    it's too complex, we should not use it any way.

--- src/ts_internal.h
+++ src/ts_internal.h
@@ -29,48 +29,6 @@
 #define TRUE 1
 #endif
 
-typedef struct ts_data_queue_s ts_data_queue_t;
-typedef struct ts_stream_block_s ts_stream_block_t;
-typedef struct ts_filter_cb_s ts_filter_cb_t;
-typedef struct ts_data_pipe_s ts_data_pipe_t;
-
-typedef int (*ts_filter_cb)(ts_data_pipe_t* dp, void* userdata);
-
-struct ts_stream_block_s {
-  char* buf;
-  int   offset;
-  int   len;
-    
-  ts_stream_block_t* prev;
-  ts_stream_block_t* next;
-};
-
-struct ts_data_queue_s {
-  ts_stream_block_t* in;
-  ts_stream_block_t* out;
-};
-
-struct ts_filter_cb_s {
-  ts_filter_cb cb;
-  
-  ts_filter_cb_t* prev;
-  ts_filter_cb_t* next;
-};
-
-struct ts_data_pipe_s {
-  ts_data_queue_t* queue;
-  ts_filter_cb_t*  filters;
-};
-
-#define TS_DATA_PIPE_IN 1
-#define TS_DATA_PIPE_OUT 0
-
-ts_data_pipe_t* ts_data_pipe__create();
-void ts_data_pipe__destroy(ts_data_pipe_t* dp);
-int ts_data_pipe__write(ts_data_pipe_t* dp, int inout, const char* data, int len);
-int ts_data_pipe__read(ts_data_pipe_t* dp, int inout, const char* data, int* len);
-int ts_data_pipe__peek(ts_data_pipe_t* dp, int inout, char** data, int *out_len);
-
 int ts_server_listener__init_default(ts_server_listener_t* listener);
 int ts_server_listener__start(ts_server_listener_t* listener, ts_server_t* server, uv_connection_cb cb);
 int ts_server_listener__stop(ts_server_listener_t* listener, uv_close_cb cb);
