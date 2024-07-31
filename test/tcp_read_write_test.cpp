6616292064040928e3ea830aae06b5f22afc6ce4
martinzrx <253951598@qq.com>
2024-07-31 17:15:00 +0800

    More unit tests.

--- test/tcp_read_write_test.cpp
+++ test/tcp_read_write_test.cpp
@@ -130,3 +130,81 @@ TEST(TCPServer, Echo2Test) {
   ts_server__stop(&server);
   
 }
+
+
+typedef struct test_echo_data_s {
+    char* recv_buf;
+    int   recv_buf_off;
+    int   to_recv;
+
+    int   client_done;
+} test_echo_data_t;
+
+static int echo_read_cb(void* ctx, ts_server_t* server, ts_conn_t* conn, const char* data, int len) {
+  test_echo_data_t* info = (test_echo_data_t*)ctx;
+
+  memcpy(info->recv_buf + info->recv_buf_off, data, len);
+  info->recv_buf_off += len;
+
+  if (info->recv_buf_off >= info->to_recv) {
+    ts_server__write(server, conn, info->recv_buf, info->recv_buf_off);
+  }
+
+  return 0;
+}
+
+static void client_large_data_cb(void *arg) {
+  int err;
+  test_echo_data_t* info = (test_echo_data_t*)arg;
+  mytcp_t client;
+  mytcp__init_mutex();
+  mytcp__init(&client);
+  err = mytcp__connect(&client, "127.0.0.1", 12345);
+  ASSERT_EQ(err, 0);
+
+  int data_len = info->to_recv;
+
+  err = mytcp__write(&client, info->recv_buf, data_len);
+  ASSERT_EQ(err, data_len);
+
+  char* recvbuf = (char*) malloc(data_len);
+  err = mytcp__read(&client, recvbuf, data_len);
+  ASSERT_EQ(err,data_len);
+
+  err = mytcp__disconnect(&client);
+  ASSERT_EQ(err, 0);
+
+  info->client_done = 1;
+}
+
+static void tcp_server__echo_large_data_impl(int data_size) {
+  test_echo_data_t info;
+  memset(&info, 0, sizeof(info));
+  info.to_recv = data_size;
+  info.recv_buf = (char*) malloc(data_size);
+  memset(info.recv_buf, 'x', data_size);
+
+  ts_server_t server;
+  start_server(&server);
+  ts_server__set_cb_ctx(&server, &info);
+  ts_server__set_read_cb(&server, echo_read_cb);
+
+  uv_thread_t client_thread;
+  uv_thread_create(&client_thread, client_large_data_cb, &info);
+
+  int r = ts_server__start(&server);
+  ASSERT_EQ(r, 0);
+
+  while (info.client_done == 0) {
+    ts_server__run(&server);
+  }
+
+  ts_server__stop(&server);
+}
+
+TEST(TCPServer, Echo1KDataTest) {
+  tcp_server__echo_large_data_impl(1024);
+}
+TEST(TCPServer, Echo10MDataTest) {
+  tcp_server__echo_large_data_impl(10 * 1024 * 1024);
+}
