e08ae5980f86f4a0441cd3d8d61ab233f5c1fccc
zzrrxx <253951598@qq.com>
2024-08-02 23:47:08 +0800

    Highlight the unit test name.

--- test/tinyunit.c
+++ test/tinyunit.c
@@ -4,26 +4,50 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef WIN32
+#include <windows.h>
+#endif
 
 #define STDOUT(format,...) \
   fprintf(stdout, format, __VA_ARGS__); \
   fflush(stdout);
+
+
+static int win_enable_virtual_terminal_processing() {
+#ifdef WIN32
+  // Set output mode to handle virtual terminal sequences
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  if (hOut == INVALID_HANDLE_VALUE) {
+    return GetLastError();
+  }
+  
+  DWORD dwMode = 0;
+  if (!GetConsoleMode(hOut, &dwMode)) {
+    return GetLastError();
+  }
+  
+  dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  if (!SetConsoleMode(hOut, dwMode)) {
+    return GetLastError();
+  }
+#endif
+}
 
 static void run_test(test_entry_t* t) {
   t->entry();
 }
 
 int run_tests() {
-  int actual;
-  int total;
-  int current;
-  int test_result;
-  int skip;
+  win_enable_virtual_terminal_processing();
+  
+  int index = 1;
   test_entry_t* test;
 
   for (test = TESTS; test->entry; test++) {
-    STDOUT("[%s]\n", test->name);
+    STDOUT("\x1b[34m##### [%d][%s]\x1b[m\n", index, test->name);
+    
     run_test(test);
+    index++;
   }
 
 }
