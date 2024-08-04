247dfc1b83bdfb7723dc91d7fe7993633d3752e3
zzrrxx <253951598@qq.com>
2024-08-04 23:15:39 +0800

    Output time used by a unit test.

--- test/tinyunit.c
+++ test/tinyunit.c
@@ -6,6 +6,10 @@
 
 #ifdef WIN32
 #include <windows.h>
+#include <Psapi.h>
+#else
+#include <sys/time.h>
+#include <sys/resource.h>
 #endif
 
 #define STDOUT(format,...) \
@@ -31,10 +35,50 @@ static int win_enable_virtual_terminal_processing() {
     return GetLastError();
   }
 #endif
+  return 0;
+}
+
+static long long get_current_time_millis() {
+#ifdef _WIN32
+  FILETIME ft;
+  LARGE_INTEGER li;
+  
+  GetSystemTimePreciseAsFileTime(&ft);
+  
+  li.LowPart = ft.dwLowDateTime;
+  li.HighPart = ft.dwHighDateTime;
+  
+  return (long long)(li.QuadPart / 10000LL - 11644473600000LL);
+#else
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (long long)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+#endif
+}
+
+long get_current_process_memory_usage() {
+#ifdef _WIN32
+  PROCESS_MEMORY_COUNTERS_EX pmc;
+  if (GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc))) {
+    return pmc.PrivateUsage;
+  } else {
+    return -1;
+  }
+#else
+  struct rusage ru;
+  if (getrusage(RUSAGE_SELF, &ru) == 0) {
+      return ru.ru_maxrss * 1024; // Convert kilobytes to bytes
+  } else {
+      return -1;
+  }
+#endif
 }
 
 static void run_test(test_entry_t* t) {
+  long long start_time = get_current_time_millis();
   t->entry();
+  long long time_used = get_current_time_millis() - start_time;
+  STDOUT("\x1b[34m##### Time used: %lld ms\x1b[m\n", time_used);
 }
 
 int run_tests() {
