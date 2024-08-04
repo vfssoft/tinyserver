26eff955cc43061e9373f03799dcca178a83b273
zzrrxx <253951598@qq.com>
2024-08-04 22:17:11 +0800

    Use uv types by pointers.

--- src/ts_server_idle.c
+++ src/ts_server_idle.c
@@ -7,19 +7,28 @@ static void uv_on_idle(uv_idle_t *handle) {
 
 int ts_server_idle__init(ts_server_t* server) {
   int err;
-  memset(&(server->uvidle), 0, sizeof(uv_idle_t));
-  err = uv_idle_init(server->uvloop, &(server->uvidle));
+  server->uvidle = (uv_idle_t*) ts__malloc(sizeof(uv_idle_t));
+  if (server->uvidle == NULL) {
+    ts_error__set(&(server->err), TS_ERR_OUT_OF_MEMORY);
+    return server->err.err;
+  }
+  memset(server->uvidle, 0, sizeof(uv_idle_t));
+  err = uv_idle_init(server->uvloop, server->uvidle);
   return err;
 }
 
 int ts_server_idle__start(ts_server_t* server) {
   int err;
-  err = uv_idle_start(&(server->uvidle), uv_on_idle);
+  err = uv_idle_start(server->uvidle, uv_on_idle);
   return err;
 }
 
 int ts_server_idle__stop(ts_server_t* server) {
-  int err;
-  err = uv_idle_stop(&server->uvidle);
+  int err = 0;
+  if (server->uvidle != NULL) {
+    err = uv_idle_stop(server->uvidle);
+    ts__free(server->uvidle);
+    server->uvidle = NULL;
+  }
   return err;
 }
