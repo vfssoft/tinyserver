935d89dbff91c87b092087ae94db9b5e8f2b7eb1
zzrrxx <253951598@qq.com>
2024-08-04 22:45:37 +0800

    close uvidle handle correctly.

--- src/ts_server_idle.c
+++ src/ts_server_idle.c
@@ -9,17 +9,29 @@ int ts_server_idle__init(ts_server_t* server) {
   int err;
   memset(&(server->uvidle), 0, sizeof(uv_idle_t));
   err = uv_idle_init(server->uvloop, &(server->uvidle));
+  if (err) {
+    ts_error__set_msg(&(server->err), err, uv_strerror(err));
+  }
   return err;
 }
 
 int ts_server_idle__start(ts_server_t* server) {
   int err;
   err = uv_idle_start(&(server->uvidle), uv_on_idle);
+  if (err) {
+    ts_error__set_msg(&(server->err), err, uv_strerror(err));
+  }
   return err;
 }
 
 int ts_server_idle__stop(ts_server_t* server) {
   int err;
   err = uv_idle_stop(&server->uvidle);
+  if (err) {
+    ts_error__set_msg(&(server->err), err, uv_strerror(err));
+    goto done;
+  }
+done:
+  uv_close((uv_handle_t*)&(server->uvidle), NULL);
   return err;
 }
