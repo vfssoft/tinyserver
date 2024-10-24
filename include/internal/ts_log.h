d2f7a6e2d02a65667cd2659bebd57e2674226e84
zzrrxx <253951598@qq.com>
2024-10-25 00:15:40 +0800

    Linux build: it builds on Ubuntu20.04 primarily

--- include/internal/ts_log.h
+++ include/internal/ts_log.h
@@ -5,6 +5,7 @@
 #include "ts_mutex.h"
 #include "ts.h"
 
+#include <stdio.h>
 #include <inttypes.h>
 
 typedef struct ts_log_s ts_log_t;
@@ -24,12 +25,12 @@ struct ts_log_s {
     ts_mutex_t mutex;
 };
 
-#define LOG(level, fmt, ...)   ts_log__log(ts_server__get_log(server), level,                __FUNCTION__, __LINE__, fmt, __VA_ARGS__)
-#define LOG_ERROR(fmt, ...)    ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_ERROR,   __FUNCTION__, __LINE__, fmt, __VA_ARGS__)
-#define LOG_INFO(fmt, ...)     ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_INFO,    __FUNCTION__, __LINE__, fmt, __VA_ARGS__)
-#define LOG_VERB(fmt, ...)     ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_VERB,    __FUNCTION__, __LINE__, fmt, __VA_ARGS__)
-#define LOG_DEBUG(fmt, ...)    ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_DEBUG,   __FUNCTION__, __LINE__, fmt, __VA_ARGS__)
-#define LOG_DEBUG_EX(fmt, ...) ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_DEBUGEX, __FUNCTION__, __LINE__, fmt, __VA_ARGS__)
+#define LOG(level, fmt, ...)   ts_log__log(ts_server__get_log(server), level,                __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...)    ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_ERROR,   __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)     ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_INFO,    __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_VERB(fmt, ...)     ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_VERB,    __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...)    ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_DEBUG,   __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG_EX(fmt, ...) ts_log__log(ts_server__get_log(server), TS_LOG_LEVEL_DEBUGEX, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
 
 int ts_log__init(ts_log_t* log, void* server);
 int ts_log__destroy(ts_log_t* log);
