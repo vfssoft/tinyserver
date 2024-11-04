35415a25cad5e5d4f6ea6387512436c6d09f5447
martinzrx <253951598@qq.com>
2024-11-04 17:39:46 +0800

    Remove mqtt from tinyserver.

--- test/testutil.h
+++ test/testutil.h
@@ -3,22 +3,13 @@
 
 #include <ts.h>
 #include <ts_internal.h>
-#include <tm.h>
-#include "test_mqtt_msgs.h"
 
-#define MQTT_PLAIN_PORT 11883
-#define MQTT_TLS_PORT   18883
-#define MQTT_WS_PORT    18080
-#define MQTT_WSS_PORT   18083
 
 #define RESET_STRUCT(s) memset(&s, 0, sizeof(s))
 
 const char* cur_dir();
 
 ts_t* start_server(int proto);
-
-tm_t* start_mqtt_server(int proto, tm_callbacks_t* cbs);
-tm_t* start_mqtt_server_custom_port(int proto, int listen_port, tm_callbacks_t* cbs);
 
 void assert_bytes_equals(const char* d1, int d1len, const char* d2, int d2len);
 
@@ -30,25 +21,5 @@ long get_current_process_memory_usage();
 void wait(int milliseconds);
 void mysleep(int milliseconds);
 
-int build_connect_pkt(
-    char* buf,
-    const char* client_id,
-    int clean_session,
-    const char* username, const char* password,
-    const char* will_msg, int will_msg_len, const char* will_topic, int will_qos, int will_retain,
-    int keep_alive
-);
-
-int build_subscribe_pkt(char* buf, int pkt_id, const char* topic, int qos);
-
-int build_publish_pkt(
-    char* buf,
-    const char* topic,
-    int pkt_id,
-    int qos, int dup, int retain,
-    const char* payload, int payload_len
-);
-
-int assert_msg(msg_t* m, const char* topic, const char* payload, int payload_len, int qos, int retained);
 
 #endif //TINYSERVER_TESTUTIL_H
